fce04efaeeed3abe7845f18d6477eb1f379602ff
apennybacker@gmail.com <apennybacker@gmail.com>
2016-08-29 15:53:00 -0400

    changed price array to a non-string, and removed items from the cart leaving only prices

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 
 //arrays will be behind the scenes
 var items = ["Banana", "Peanut butter", "Wine", "Vodka"];
-var prices = ["$0.39", "$4.69", "$15.82", "$30.00" ];
+var prices = [0.39, 4.69, 15.82, 30.00 ];//removed strings to enable calculation
 var cart = [];
 
 function addBanana(){
-	cart.push(items[0], prices[0]);
+	cart.push(prices[0]);//removed item from cart; only adding price
 	console.log(cart);
 }
 
 function addPeanutButter(){
-	cart.push(items[1], prices[1]);
+	cart.push(prices[1]);//removed item from cart; only adding price
 	console.log(cart);
 }
 
 function addWine(){
-	cart.push(items[2], prices[2]);
+	cart.push(prices[2]);//removed item from cart; only adding price
 	console.log(cart);
 }
 
 function addVodka(){
-	cart.push(items[3], prices[3]);
+	cart.push(prices[3]);//removed item from cart; only adding price
 	console.log(cart);
 }
 
 function checkout(){
-	//console.log("Hello, Amy");//testing to see if checkout works
-	var total = 0;
+var total = 0;	
 	for (var i = 0; i<cart.length; i++){
-		//console.log("Testing");//not making it this far...
 		total = total + cart[i];
 		//cart.push(total);
 		cart.clear;
-	}console.log(total);//moved print out of the loop
+	}console.log("$" + total);//moved print out of the loop
 }
 
 
